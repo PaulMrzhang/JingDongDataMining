9d5bb4b0c129cfe6114e667e089c1c192892be12
paulmrzhang <1016178729@qq.com>
2016-05-24 17:28:00 +0800

    问题记录

--- JD/src/cn/edu/pku/ss/jddatamining/util/GlobalConfig.java
+++ JD/src/cn/edu/pku/ss/jddatamining/util/GlobalConfig.java
@@ -2,9 +2,9 @@ package cn.edu.pku.ss.jddatamining.util;
 
 public class GlobalConfig {
 	public static String DBUserName = "root";
-	public static String DBPassword = "123456";
+	public static String DBPassword = "root";
 	public static String DBHost = "localhost";
-	public static String DBName = "jdcrawler2";
+	public static String DBName = "jdcrawler";
 	public static String ProjacePath = "";
 	public static long timeThres = 50000;//5000ms
 }
